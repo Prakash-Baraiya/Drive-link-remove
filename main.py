1d797676990e3c6adc12b483277a99186e719186
Prakash-Baraiya <133520032+Prakash-Baraiya@users.noreply.github.com>
2023-09-06 18:57:57 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+# Replace 'YOUR_BOT_TOKEN' with your actual bot token obtained from BotFather on Telegram
+bot_token = 'YOUR_BOT_TOKEN'
+
+def start(update, context):
+    update.message.reply_text("Please send a .txt file containing name:url pairs.")
+
+def process_txt_file(update, context):
+    file = update.message.document.get_file()
+    file.download('input.txt')
+    with open('input.txt', 'r') as infile:
+        lines = infile.readlines()
+
+    # Filter out lines containing 'cpvod.testbook' or 'drive.google.com'
+    filtered_lines = [line for line in lines if 'cpvod.testbook' not in line and 'drive.google.com' not in line]
+
+    # Remove empty lines
+    filtered_lines = [line.strip() for line in filtered_lines if line.strip()]
+
+    # Prepare the corrected content
+    corrected_content = '\n'.join(filtered_lines)
+
+    # Write the corrected content to a new file
+    with open('output.txt', 'w') as outfile:
+        outfile.write(corrected_content)
+
+    # Send the corrected file to the user
+    context.bot.send_document(chat_id=update.effective_chat.id, document=open('output.txt', 'rb'))
+    os.remove('input.txt')
+    os.remove('output.txt')
+
+def main():
+    updater = Updater(bot_token, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler(Filters.document.file_extension('txt'), process_txt_file))
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
